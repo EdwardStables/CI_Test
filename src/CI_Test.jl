3160c64c4588ede51a5735e741b6679b084796be
EdwardStables <edward.stables1198@gmail.com>
2020-04-12 14:53:03 +0100

    tests and docs

--- src/CI_Test.jl
+++ src/CI_Test.jl
@@ -1,5 +1,38 @@
 module CI_Test
 
-greet() = print("Hello World!")
+
+"""
+    TerribleGreek()
+
+Map lowercase latin characters to symbols that kinda resemble them, ish...
+"""
+function TerribleGreek()
+    return Dict('a' => 'α',
+                'b' => 'β',
+                'c' => '≪',
+                'd' => 'δ',
+                'e' => '∈',
+                'f' => 'ɾ',
+                'g' => 'ʔ',
+                'h' => 'ƕ',
+                'i' => 'ι',
+                'j' => 'ȷ',
+                'k' => 'k',
+                'l' => 'ł',
+                'm' => 'μ',
+                'n' => 'π',
+                'o' => 'ø',
+                'p' => 'ɤ',
+                'q' => '♩',
+                'r' => 'ρ',
+                's' => '𝖲',
+                't' => 'τ',
+                'u' => '⟰',
+                'v' => '∇',
+                'w' => '≅',
+                'x' => 'Ξ',
+                'y' => '¥',
+                'z' => 'ζ',)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,10 @@ using Test
 
 @testset "CI_Test.jl" begin
     # Write your own tests here.
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    d = CI_Test.TerribleGreek()
+    @test typeof(d) == Dict{Char,Char}
+    for c in keys(d)
+        @test c in alphabet
+    end
 end
